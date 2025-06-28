dbbf77ef4f9ad50a7480fe88c2fdbb6b0c0714c4
Talash Hembram <talashhembram@Talashs-MacBook-Air.local>
2025-06-28 18:58:06 +0530

    fix: update CORS config for production frontend

--- index.js
+++ index.js
@@ -15,20 +15,24 @@ const PORT = process.env.PORT || 5050;
 
 const allowedOrigins = [
   'https://chat-frontend-pied-eight.vercel.app',
-  'http://localhost:5173'
+  'http://localhost:5173',
 ];
 
 app.use(cors({
   origin: function (origin, callback) {
-   
+    
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true,
 }));
+
+app.options('*', cors()); 
+
+
 app.use(express.json());
 
 
